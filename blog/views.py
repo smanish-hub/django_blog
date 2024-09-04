4ca245c6b333fb7f2e25993ef3276fc0e6634b70
Manish <manish.swarnakar266@gmail.com>
2024-09-04 18:27:33 +0530

    Completed Web Part 6 -user registration

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    context={
+        'posts':Post.objects.all()
+    }
+    return render(request,'blog/home.html',context)
+
+def about(request):
+    return render(request,'blog/about.html',{'title':'About'})
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'users',
+    'crispy_forms',     #crispy forms app
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

5e13df3f56a2f471cf50122bde4d0929d49618c1
Manish <manish.swarnakar266@gmail.com>
2024-09-06 22:57:25 +0530

    Completed Part 9-Update User Profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
@@ -8,3 +9,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save()
+        image=Image.open(self.image.path)
+
+        if image.height>300 and image.width>300:
+            image.thumbnail((300,300))
+            image.save(self.image.path)
+
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 # Create your views here. 
 
@@ -19,4 +19,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    
+    if request.method == 'POST':
+        
+        #here when the user click on the update link the form data will be updated
+        user_form=UserUpdateForm(request.POST,instance=request.user)
+        profile_form=ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        #checking the weather form is valid or not
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:   
+        #here when the user click on the profile link the form data will be prefilled with the actual data
+        user_form=UserUpdateForm(instance=request.user)
+        profile_form=ProfileUpdateForm(instance=request.user.profile)
+
+    context={
+        'u_form':user_form,
+        'p_form':profile_form
+    }
+    return render(request,'users/profile.html',context)

d388a5de8b20e92b24fa8a8ef40e49574ce3bbfb
Manish <manish.swarnakar266@gmail.com>
2024-09-08 18:31:11 +0530

    Completed Part 10-Create,Update and Delete Post

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +13,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title #object name will be replace by the title
+    
+    #this will redirect the url to post-detail when the CreateView is submitted
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 # Create your views here.
 
 
@@ -9,6 +17,59 @@ def home(request):
         'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
+
+
+class PostListView(ListView):
+    model= Post
+    template_name='blog/home.html' #<app>/<model_name>_<view_type>.html
+    context_object_name='posts'    #this is used for passing the context in the template
+
+    ordering='-date_posted'        #the '-' prefix before the column name by which ordering is to be done denotes DESC order 
+
+
+class PostDetailView(DetailView):
+    model=Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model=Post
+    fields=['title','content']
+
+    #to save the prevent the intergity error we are setting the instance of author by overriding form_valid method
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user #assigning the intance with current logged in user
+        return super().form_valid(form)        #overiding the parent class with the new instace
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post
+    fields=['title','content']
+
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user #assigning the intance with current logged in user
+        return super().form_valid(form)        #overiding the parent class with the new instace
+
+    #to check weather the user is same or not while updating the blog
+    def test_func(self):
+        post=self.get_object()  #it will get the object of the Post model
+
+        if self.request.user ==  post.author:   #using that object we are verfying the user and author are same or not
+            return True
+        return False        #if not same 403 Forbidden error will be returned
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+
+    model=Post
+    success_url='/' #on click of yes it will take you to the home page
+    
+    #to check weather the user is same or not while updating the blog
+    def test_func(self):
+        post=self.get_object()  #it will get the object of the Post model
+
+        if self.request.user ==  post.author:   #using that object we are verfying the user and author are same or not
+            return True
+        return False        #if not same 403 Forbidden error will be returned
+
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
